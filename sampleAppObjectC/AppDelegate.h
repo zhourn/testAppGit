6402ad946fb65220f43474e887f05e560ec1a0be
周荣 <zhourn@hotmail.com>
2017-01-07 20:19:31 +0800

    Initial Commit

---
+++ sampleAppObjectC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleAppObjectC
+//
+//  Created by zhourong on 17/1/7.
+//  Copyright © 2017年 zhourong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
