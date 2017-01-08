ff26f2d6c7616e5496b31c3e8127df486adcc652
zhourong <zhourn@hotmail.com>
2017-01-08 18:31:53 +0800

    first commit

--- sampleAppObjectC/ViewController.m
+++ sampleAppObjectC/ViewController.m
@@ -14,8 +14,23 @@
 
 @implementation ViewController
 
+- (void)createUI {
+    UILabel* label = [[UILabel alloc] init];
+    
+    label.text = @"我的第一个APP程序";
+    
+    label.frame = CGRectMake(100, 100, 160, 40);
+    
+    label.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:label];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self createUI];
+    // NSLog(@"这是我的第一个APP程序");
+    // self.view.backgroundColor = [UIColor redColor];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
